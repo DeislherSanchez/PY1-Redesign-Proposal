cd20e4a0fdf486d2c642109ecee7cf8aa2f16c8f
Deislher Yadir Sánchez Funez <125695457+DeislherSanchez@users.noreply.github.com>
2025-10-06 02:12:55 -0600

    Intento solucion scroll horizontal moviles
    Se ajusto el app.vue

--- RedesignProposal/src/App.vue
+++ RedesignProposal/src/App.vue
@@ -7,3 +7,22 @@ import { RouterView } from 'vue-router';
   <RouterView/>
 </template>
 
+<style>
+/* Prevenir scroll horizontal en toda la aplicación */
+html, body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+  max-width: 100%;
+}
+
+#app {
+  overflow-x: hidden;
+  max-width: 100vw;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
+
