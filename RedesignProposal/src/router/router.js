b84a1abcc2a6dbdee1ce3cba7cb531f3d97ab852
Deislher Yadir Sánchez Funez <125695457+DeislherSanchez@users.noreply.github.com>
2025-10-06 00:56:50 -0600

    Ajustes
    3er intento para verlo en netlify

--- RedesignProposal/src/router/router.js
+++ RedesignProposal/src/router/router.js
@@ -34,7 +34,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // Siempre scroll al inicio de la página en cada navegación
